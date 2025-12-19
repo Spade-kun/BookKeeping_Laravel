f1a9901ce32b448f120b9f8d589d69ce3f228820
Spade-kun <2201104232@student.buksu.edu.ph>
2025-12-20 07:35:44 +0800

    Fix production asset loading - use manifest when available

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,9 +45,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     
     <!-- Vite Assets -->
-    @if(env('VITE_DEV_SERVER_ENABLED', true))
+    @if(app()->environment('local') && env('VITE_DEV_SERVER_ENABLED', false))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
+    @elseif(file_exists(public_path('build/manifest.json')))
         @php
             $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
             $cssFile = $manifest['resources/css/app.css']['file'] ?? '';
@@ -57,8 +57,10 @@
             <link rel="stylesheet" href="{{ asset('build/' . $cssFile) }}">
         @endif
         @if($jsFile)
-            <script type="module" src="{{ asset('build/' . $jsFile) }}" defer></script>
+            <script type="module" src="{{ asset('build/' . $jsFile) }}"></script>
         @endif
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
     
     @stack('styles')
